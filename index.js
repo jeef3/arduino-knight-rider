75a974ca31faddc9d31803bbfa8b332aae67ff00
Jeff Knaggs <mail@jeef3.com>
2015-03-29 14:29:17 +1300

    Refactor

--- index.js
+++ index.js
@@ -3,31 +3,33 @@ var five = require('johnny-five');
 var board = new five.Board();
 board.on('ready', function () {
   var _self = this;
-  var direction = 1;
+  var startPosition = 0;
+  var startDirection = 1;
+
   var pins = [3, 5, 6, 9, 10];
   var tailLength = 1;
 
-  var getState = function (curr) {
+  var getState = function (vector) {
     var state = {
       tail: [],
-      on: pins[curr]
+      on: pins[vector.position]
     };
 
     state.off = pins.slice(0);
     state.off.splice(state.off.indexOf(state.on), 1);
 
     var index, i;
-    if (direction && curr > 0) {
+    if (vector.direction && vector.position > 0) {
       for (i = 0; i < tailLength; i++) {
-        index = curr - (i + 1);
+        index = vector.position - (i + 1);
         state.tail.push(pins[index]);
         state.off.splice(state.off.indexOf(pins[index]), 1);
       }
     }
 
-    if (!direction && curr < pins.length - 1) {
+    if (!vector.direction && vector.position < pins.length - 1) {
       for (i = 0; i < tailLength; i++) {
-        index = curr + (i + 1);
+        index = vector.position + (i + 1);
         state.tail.push(pins[index]);
         state.off.splice(state.off.indexOf(pins[index]), 1);
       }
@@ -36,34 +38,47 @@ board.on('ready', function () {
     return state;
   };
 
-  var move = function(curr) {
-    var state = getState(curr);
-
+  var renderState = function (state) {
     five.Leds(state.off).off();
     five.Leds(state.tail).brightness(30);
     five.Led(state.on).on();
+  };
 
-    _self.wait(150, function () {
-      var shift;
-      if (direction) {
-        shift = curr + 1;
-      } else {
-        shift = curr - 1;
-      }
+  var getNextVector = function (vector) {
+    var newVector = {
+      direction: vector.direction
+    };
 
-      if (shift > pins.length - 1) {
-        shift = pins.length - 2;
-        direction = 0;
-      }
+    if (vector.direction) {
+      newVector.position = vector.position + 1;
+    } else {
+      newVector.position = vector.position - 1;
+    }
 
-      if (shift < 0) {
-        shift = 1;
-        direction = 1;
-      }
+    if (newVector.position > pins.length - 1) {
+      newVector.position = pins.length - 2;
+      newVector.direction = 0;
+    }
 
-      move(shift);
+    if (newVector.position < 0) {
+      newVector.position = 1;
+      newVector.direction = 1;
+    }
+
+    return newVector;
+  };
+
+  var next = function(vector) {
+    var state = getState(vector);
+    renderState(state);
+
+    _self.wait(550, function () {
+      next(getNextVector(vector));
     });
   };
 
-  move(0);
+  next({
+    position: startPosition, 
+    direction: startDirection
+  });
 });
